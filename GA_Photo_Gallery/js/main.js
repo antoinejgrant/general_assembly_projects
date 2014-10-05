43b29299f30eec5734238feda5674d1a495e676b
ZackDinerstein <zackdhome@gmail.com>
2014-10-05 15:59:59 -0700

    Assignment #5 - Slideshow - Feedback

--- GA_Photo_Gallery/js/main.js
+++ GA_Photo_Gallery/js/main.js
@@ -1,7 +1,6 @@
 var images = ["img/thumb_0.jpg", "img/thumb_1.jpg", "img/thumb_2.jpg", "img/thumb_3.jpg", "img/thumb_4.jpg"];
 var currentImage = 0;	
 
-
 function changePicture(whichNum) {
 	var img = document.getElementById('mainImage');
 	img.setAttribute('src', images[whichNum]);
@@ -10,7 +9,7 @@ function changePicture(whichNum) {
 
 function nextImage() {
 	currentImage++;
-	if(currentImage===images.length){
+	if(currentImage === images.length){
 		currentImage = 0;
 	}
 	var img = document.getElementById('mainImage');
@@ -19,7 +18,7 @@ function nextImage() {
 
 function prevImage() {
 	currentImage = currentImage -1;
-	if(currentImage===-1){
+	if(currentImage === -1){
 		currentImage=(images.length-1);
 	}
 	var img = document.getElementById('mainImage');
