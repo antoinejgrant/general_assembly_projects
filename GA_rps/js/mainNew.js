9d8eb4cf5cfcc979cc9ed996bafa615cb6574995
lordtwon@gmail.com <Antoine@Antoine-Grants-MacBook-Pro.local>
2014-10-07 20:31:54 -0400

    more optimized rock paper scissors game

---
+++ GA_rps/js/mainNew.js
@@ -0,0 +1,47 @@
+var choices = ["rock", "paper", "scissors"];
+var playerScore = 0;
+var computerScore = 0;
+
+var playerWins = ["rockscissors", "paperrock", "scissorspaper"];
+
+
+function choice(playerChoice){
+	var computerChoice = choices[Math.floor(Math.random()*choices.length)];
+	var msg;
+	
+	if(playerChoice == computerChoice){
+		msg = "Draw";
+ 	} 
+  	else{
+    	var outcome = playerChoice + computerChoice;
+    	$.each(playerWins, function(index, value){
+      		if(outcome == value){
+       		msg = "User";
+        	} 
+    	});
+    }   	
+
+	if(msg == "Draw"){ 
+		document.getElementById('title').innerHTML="draw - you both played " + computerChoice;
+		titleBar.style.backgroundColor = "F39C12";
+		home.style.backgroundColor = "#ECF0F1";
+		away.style.backgroundColor = "#ECF0F1";
+	}
+	else if(msg == "User"){
+	    playerScore ++;
+	    document.getElementById('title').innerHTML="you win - computer played " + computerChoice;
+	    titleBar.style.backgroundColor = "16A085";
+		home.style.backgroundColor = "#16A085";
+		away.style.backgroundColor = "#C0392B";
+	}
+	else {
+		computerScore ++;
+	    document.getElementById('title').innerHTML="you lose - computer played " + computerChoice;
+	    titleBar.style.backgroundColor = "C0392B";
+		home.style.backgroundColor = "#C0392B";
+		away.style.backgroundColor = "#16A085";
+	}
+
+	document.getElementById('homeScore').innerHTML = playerScore;
+	document.getElementById('awayScore').innerHTML = computerScore;
+}
